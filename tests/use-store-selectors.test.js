681a9600597a7485a82dc71b1ab5120b38753371
Christophe <contact@christophedeveaux.com>
2025-04-01 17:49:49 +0000

    feat: enhance rule to support additional store names

--- tests/use-store-selectors.test.js
+++ tests/use-store-selectors.test.js
@@ -6,7 +6,10 @@ const ruleTester = new RuleTester({
 });
 ruleTester.run('use-store-selectors', rule, {
   valid: [
-    'count = useStore(state => state.count);'
+    'count = useStore(state => state.count);',
+    'count = useCountStore(state => state.count);',
+    'const { count, increment } = useStore(state => ({ count: state.count, increment: state.increment }), shallow);',
+    'const { count, increment } = useCountStore(state => ({ count: state.count, increment: state.increment }), shallow);'
   ],
   invalid: [
     {
@@ -14,8 +17,16 @@ ruleTester.run('use-store-selectors', rule, {
       errors: [{ message: 'You should use selectors when calling useStore to improve performance.' }]
     },
     {
-      code: 'const store = useStore(state => state.count, anotherState => anotherState.value);',
+      code: 'count = useCountStore();',
       errors: [{ message: 'You should use selectors when calling useStore to improve performance.' }]
+    },
+    {
+      code: 'count = useStore(state => ({ count: state.count }));',
+      errors: [{ message: 'You should use `shallow` as a second parameter when returning object.' }]
+    },
+    {
+      code: 'count = useCountStore(state => ({ count: state.count }));',
+      errors: [{ message: 'You should use `shallow` as a second parameter when returning object.' }]
     }
   ]
 });
